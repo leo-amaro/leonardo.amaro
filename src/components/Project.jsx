df3ccd406e6b26f0cccac2b3ae230d05833265e2
leo-amaro <leo.munoz.amaro@gmail.com>
2025-06-21 19:11:59 -0400

    update resume link in Experience component and comment out project archive link in Projects component for future reference

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -70,7 +70,7 @@ export default function Projects() {
                         </li>
                     ))}
                 </ul>
-                <div className="mt-12">
+                {/* <div className="mt-12">
                     <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="#" target="_blank" rel="noreferrer noopener" aria-label="View Full Project Archive (opens in a new tab)">
                         <span>View Full <span className="inline-block">Project Archive
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
@@ -78,7 +78,7 @@ export default function Projects() {
                             </svg>
                         </span></span>
                     </a>
-                </div>
+                </div> */}
             </div>
         </section>
     );
